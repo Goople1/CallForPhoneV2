c9d89741292b1ca4bb26a4dc255c97e51c18bec3
Goople1 <jsoteloj@upao.edu.pe>
2016-01-26 01:05:23 -0500

    order

--- jkProducto/content/static/js/sucursal/mantenimientoSucursal.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-      
-      $( ".leftpanel .leftpanelinner" ).on( "click", ".nav-parent", function() {
-          var menuSeleccionado=$(this).prop('id');
-          $(".menuGeneral").addClass('hidden');
-          $("."+menuSeleccionado).removeClass('hidden');
-
-      });
-  });
