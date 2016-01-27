3afde2253ba9638f402555e0bc2770d560e89195
Goople1 <jsoteloj@upao.edu.pe>
2016-01-27 16:15:53 -0500

    url

--- jkProducto/ventas/urls.py
+++ jkProducto/ventas/urls.py
@@ -4,22 +4,22 @@ from ventas import views
 urlpatterns = patterns('',
 
 	url(r'^$',views.home_ventas , name='home_ventas'),
-	url(r'^realizarventa/$',views.venta , name='venta'),
-	url(r'^addVenta/$',views.addVenta , name='addVenta'),
+	url(r'^(?i)realizarventa/$',views.venta , name='venta'),
+	url(r'^(?i)addVenta/$',views.addVenta , name='addVenta'),
 	
-	url(r'^modificar/$',views.modificar_venta, name='modificar'),
-	url(r'^modificar/(?P<venta_id>[0-9]+)/$',views.modificar_detalle_ventas, name='detalleventa'),
+	url(r'^(?i)modificar/$',views.modificar_venta, name='modificar'),
+	url(r'^(?i)modificar/(?P<venta_id>[0-9]+)/$',views.modificar_detalle_ventas, name='detalleventa'),
 
 	#url(r'^reporte/asistencia/$',views.reporte_asistencia, name='reporteAsistencia'),
-	url(r'^reporte/stock/$',views.lista_producto , name='reporteStock'),
-	url(r'^reporte/venta/$',views.reporte_venta , name='reporteVenta'),
-	url(r'^reporte/venta/detalle/$',views.detalle_venta , name='reporteDetalleVenta'),
+	url(r'^(?i)reporte/stock/$',views.lista_producto , name='reporteStock'),
+	url(r'^(?i)reporte/venta/$',views.reporte_venta , name='reporteVenta'),
+	url(r'^(?i)reporte/venta/detalle/$',views.detalle_venta , name='reporteDetalleVenta'),
 
 
-	url(r'^cargarProductos/$',views.cargar_productos, name='cargar_productos'),
-	url(r'^asistencia/$',views.asistencia , name='asistencia'),
+	url(r'^(?i)cargarProductos/$',views.cargar_productos, name='cargar_productos'),
+	url(r'^(?i)asistencia/$',views.asistencia , name='asistencia'),
 	#url(r'^reporte/$',views.home_empleado_menu_reporte , name='menuReporte'),
-	url(r'^home/$',views.home_empleado_menu_reporte , name='home'),
+	url(r'^(?i)home/$',views.home_empleado_menu_reporte , name='home'),
 
 
 
